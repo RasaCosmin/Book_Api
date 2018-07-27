3c1b7a69ac4156a12f1b485c0ef0e2a02daddf39
Rasa Cosmin <rasa.cosmin@gmail.com>
2018-07-27 15:13:07 +0300

    intial configs

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI: 'mongodb://rasacosmin:P%40ssw0rd@ds257241.mlab.com:57241/book_db',
+	secret: 'secret'
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const passport = require('passport');
+
+const app = express();
+
+app.use(logger('dev'));
+
+//body parser
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
+app.use(bodyParser.json());
+
+const dbConfig = require('./config/keys').mongoURI;
+mongoose
+	.connect(
+		dbConfig,
+		{ useNewUrlParser: true }
+	)
+	.then(() => console.log('db connected'))
+	.catch(err => console.log(err));
+
+app.use(passport.initialize());
+//passport config
+//require('./config/passport')(passport);
+
+// app.use('/api', users);
+// app.use('/api/list', shoppingList);
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`server run on port ${port}`));
