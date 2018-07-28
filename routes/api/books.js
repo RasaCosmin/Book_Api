e980a118563e36674728c9cc6689d073ea0b7927
Rasa Cosmin <rasa.cosmin@gmail.com>
2018-07-28 15:36:53 +0300

    add new methods on books

--- routes/api/books.js
+++ routes/api/books.js
@@ -12,7 +12,7 @@ router.get(
 	passport.authenticate('jwt', { session: false }),
 	(req, res) => {
 		Book.find().then(books => {
-			if (!books) {
+			if (!books || books.length == 0) {
 				res.status(404).json({
 					error: 'No book in db'
 				});
@@ -38,8 +38,47 @@ router.post(
 
 		newBook
 			.save()
-			.then(book => res.json({ message: 'success' }))
-			.catch(err => res.jsson({ error: 'Failed to add a new book' }));
+			.then(book => res.json(book))
+			.catch(() => res.json({ error: 'Failed to add a new book' }));
+	}
+);
+
+//@route PUT /api/books/:id
+//@desc Update book
+//@access private
+router.put(
+	'/:id',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		const bookFields = {};
+		if (req.body.title) bookFields.title = req.body.title;
+		if (req.body.author) bookFields.author = req.body.author;
+		if (req.body.publisher) bookFields.publisher = req.body.publisher;
+
+		Book.findById(req.params.id).then(book => {
+			if (!book) {
+				res.status(404).json({ error: 'No book with this id' });
+			} else {
+				Book.findOneAndUpdate(
+					{ _id: req.params.id },
+					{ $set: bookFields },
+					{ new: true }
+				).then(book => res.json(book));
+			}
+		});
+	}
+);
+
+//@route DELETE /api/books/:id
+//@desc DELETE book
+//@access private
+router.delete(
+	'/:id',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Book.findOneAndRemove({ _id: req.params.id }).then(book => {
+			res.send({ success: true });
+		});
 	}
 );
 
